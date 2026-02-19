b198c1e8ffa2b554c7b2fefa7484d827aeb5aa0e
Prasun-Budd15 <prasun.buddhacharya.a24@icp.edu.np>
2026-02-19 11:56:24 +0545

    Add topic

--- src/main/java/com/ingcollegeapt/week1tut/Week1Tut.java
+++ src/main/java/com/ingcollegeapt/week1tut/Week1Tut.java
@@ -1,22 +1,38 @@
 package com.ingcollegeapt.week1tut;
 
-import java.util.Scanner;
 
-public class Week1Tut {
+import com.ingcollegeapt.week1tut.model.Topic;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Week1Tut{
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final List<Topic> topics = new ArrayList();
 
     public static void main(String[] args) {
         while (true) {
             showMenu();
             String choice = scanner.next();
 
-            switch (choice) {
+            switch (choice){
                 case "1":
                     // TODO: Add topic
+                    System.out.println("Enter a topic name:");
+                    String topic_name = scanner.next();
+                    int topic_id = topics.size() + 1;
+                    Topic newTopic = new Topic(topic_id, topic_name);
+                    topics.add(newTopic);
+                    System.out.println("Topic added succesfully");
                     break;
                 case "2":
                     // TODO: View topics
+                    for (Topic topic : topics){
+                        System.out.println("ID : " + topic.getTopic_id() + ", Name: " + topic.getTopic_name());
+                    }
                     break;
                 case "3":
                     // TODO: Add entry to a topic
--- src/main/java/com/ingcollegeapt/week1tut/model/Topic.java
+++ src/main/java/com/ingcollegeapt/week1tut/model/Topic.java
@@ -1,9 +1,49 @@
 package com.ingcollegeapt.week1tut.model;
 
 //Create a POJO Class for Topic
+import java.time.LocalDateTime;
 /*
 It has id, name, created and updated TimeStamp
 */
 public class Topic {
+    private int topic_id;
+    private String topic_name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Topic(int topic_id, String topic_name) {
+        this.topic_id = topic_id;
+        this.topic_name = topic_name;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getTopic_id() {
+        return topic_id;
+    }
+
+    public void setTopic_id(int topic_id) {
+        this.topic_id = topic_id;
+        this.updatedAt=LocalDateTime.now(); 
+    }
+
+    public String getTopic_name() {
+        return topic_name;
+    }
+
+    public void setTopic_name(String topic_name) {
+        this.topic_name = topic_name;
+        this.updatedAt=LocalDateTime.now(); 
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    
+    
     
 }
