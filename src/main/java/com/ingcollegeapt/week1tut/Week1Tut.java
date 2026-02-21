6a58745ea83d9cc26b2c15fb81d6aa8832ac1eed
Prasun-Budd15 <prasun.buddhacharya.a24@icp.edu.np>
2026-02-21 08:35:37 +0545

    Update Testing done

--- src/main/java/com/ingcollegeapt/week1tut/Week1Tut.java
+++ src/main/java/com/ingcollegeapt/week1tut/Week1Tut.java
@@ -14,12 +14,18 @@ public class Week1Tut{
         while (true) {
             showMenu();
             String choice = scanner.next();
-
+            try{
             switch (choice){
                 case "1":
                     // TODO: Add topic
                     System.out.println("Enter a topic name:");
-                    String topic_name = scanner.next();
+                           scanner.nextLine();
+                    String topic_name = scanner.nextLine();
+             
+                             if(topic_name.isEmpty()){
+                                System.out.println("NO topic Added");
+                                break;
+                    }
                     int topic_id = topics.size() + 1;
                     Topic newTopic = new Topic(topic_id, topic_name);
                     topics.add(newTopic);
@@ -31,9 +37,10 @@ public class Week1Tut{
                         System.out.println("No Topic recorded");
                     }
                         else{
+                            System.out.println("------------ xTOPICx ------------------");
                     for (Topic topic : topics){
-                        System.out.println("------------ xTOPICx ------------------");
                         System.out.println("ID      : " + topic.getTopic_id() + "\nName    : " + topic.getTopic_name() +"\nCreated : " +topic.getCreatedAt() +"\nUpdated : " +topic.getUpdatedAt());
+                        System.out.println();
                     }
                         System.out.println("*******************--*******************");
                     break;
@@ -51,13 +58,13 @@ public class Week1Tut{
                 default:
                     System.out.println("Invalid choice.");
             }
-            
-            
-            
-           
         }
-    }
-
+            catch(Exception e){
+                System.out.println("Error: Please Choose a valid option");
+        }
+        }
+        }
+    
     private static void showMenu() {
         System.out.println("\nLearning Logs Menu");
         System.out.println("1. Add Topic");
